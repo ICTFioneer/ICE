7b751dd2366a586054ecc0c86c2cc72b886ab5fe
ICTFioneer <hopsiauto@gmail.com>
2025-12-25 23:06:48 +0100

    Update DetailL2.controller.js

--- DetailL2.controller.js
+++ DetailL2.controller.js
@@ -1,94 +1,69 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel",
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
   "sap/ui/core/routing/History"
-], function (Controller, JSONModel, Filter, FilterOperator, History) {
+], function (Controller, Filter, FilterOperator, History) {
   "use strict";
 
   return Controller.extend("ice.controller.DetailL2", {
 
     onInit: function () {
-      this.getView().setModel(new JSONModel({
-        navKeys: {
-          ICERunDate: null,
-          CompanyCode: "",
-          TradingPartner: "",
-          FinalBreakCode: ""
-        },
-        drill: {
-          field: "",
-          value: ""
-        }
-      }), "vm");
-
       this._sfbReady = false;
-      this._stReady = { stProductL2: false, stSegmentL2: false, stCaptionL2: false };
-      this._pendingRebind = false;
-      this._pendingRebindBySt = { stProductL2: false, stSegmentL2: false, stCaptionL2: false };
+      this._stReady = false;
+      this._pending = false;
+
+      this._navKeys = {};
+      this._drill = { es: "", field: "", value: "" };
 
       var oSfb = this.byId("sfbDetailL2");
       if (oSfb) {
         oSfb.attachInitialise(function () {
           this._sfbReady = true;
           this._restoreSfbFromSession();
-          if (this._pendingRebind) {
-            this._pendingRebind = false;
-            this._rebindActiveSafe();
-          }
+          if (this._pending) { this._pending = false; this._applyAndRebind(); }
         }.bind(this));
       }
 
-      this._attachSmartTableInit("stProductL2");
-      this._attachSmartTableInit("stSegmentL2");
-      this._attachSmartTableInit("stCaptionL2");
+      var oSt = this.byId("stDetailL2");
+      if (oSt) {
+        oSt.attachInitialise(function () {
+          this._stReady = true;
+          if (this._pending) { this._pending = false; this._applyAndRebind(); }
+        }.bind(this));
+      }
 
       this.getOwnerComponent().getRouter()
         .getRoute("RouteDetailL2")
         .attachPatternMatched(this._onRouteMatched, this);
     },
 
-    _attachSmartTableInit: function (sId) {
-      var oSt = this.byId(sId);
-      if (!oSt) { return; }
-
-      oSt.attachInitialise(function () {
-        this._stReady[sId] = true;
-        if (this._pendingRebindBySt[sId]) {
-          this._pendingRebindBySt[sId] = false;
-          this._rebindActiveSafe();
-        }
-      }.bind(this));
-    },
-
     _onRouteMatched: function (oEvent) {
-      var oArgs = oEvent.getParameter("arguments") || {};
-      var oQuery = oArgs["?query"] || {};
+      var a = oEvent.getParameter("arguments") || {};
+      var q = a["?query"] || {};
 
-      var sIceIso = decodeURIComponent(oArgs.ICERunDate || "");
+      var sIceIso = decodeURIComponent(a.ICERunDate || "");
       var dIce = sIceIso ? new Date(sIceIso) : null;
 
-      this.getView().getModel("vm").setProperty("/navKeys", {
-        ICERunDate: dIce && !isNaN(dIce.getTime()) ? dIce : null,
-        CompanyCode: decodeURIComponent(oArgs.CompanyCode || ""),
-        TradingPartner: decodeURIComponent(oArgs.TradingPartner || ""),
-        FinalBreakCode: decodeURIComponent(oArgs.FinalBreakCode || "")
-      });
+      this._navKeys = {
+        ICERunDate: (dIce && !isNaN(dIce.getTime())) ? dIce : null,
+        CompanyCode: decodeURIComponent(a.CompanyCode || ""),
+        TradingPartner: decodeURIComponent(a.TradingPartner || ""),
+        FinalBreakCode: decodeURIComponent(a.FinalBreakCode || "")
+      };
 
-      var sTab = (oQuery.tab ? decodeURIComponent(oQuery.tab) : "product") || "product";
-      this.byId("itbViewsL2").setSelectedKey(sTab);
+      this._drill = {
+        es: decodeURIComponent(q.es || "TeamProductViewL2Set"),
+        field: decodeURIComponent(q.field || ""),
+        value: q.value ? decodeURIComponent(q.value) : ""
+      };
 
-      this.getView().getModel("vm").setProperty("/drill", {
-        field: decodeURIComponent(oQuery.field || ""),
-        value: decodeURIComponent(oQuery.value || "")
-      });
-
-      if (this._sfbReady) {
-        this._restoreSfbFromSession();
+      if (!this._sfbReady || !this._stReady) {
+        this._pending = true;
+        return;
       }
 
-      this._rebindActiveSafe();
+      this._applyAndRebind();
     },
 
     _restoreSfbFromSession: function () {
@@ -97,64 +72,50 @@ sap.ui.define([
 
       try {
         var sData = sessionStorage.getItem("MAIN_SFB_FILTERS");
-        if (!sData) { return; }
-
-        oSfb.setFilterData(JSON.parse(sData), true);
+        if (sData) {
+          oSfb.setFilterData(JSON.parse(sData), true);
+        }
       } catch (e) {
         // ignore
       }
     },
 
-    onBeforeRebindAny: function (oEvent) {
-      var p = oEvent.getParameter("bindingParams");
-      p.filters = (p.filters || []).concat(this._getAllFilters());
+    _applyAndRebind: function () {
+      // 1) prebaci entitySet prema kliku
+      var oSt = this.byId("stDetailL2");
+      if (oSt && this._drill.es) {
+        oSt.setEntitySet(this._drill.es);
+      }
+
+      // 2) rebind
+      oSt && oSt.rebindTable(true);
     },
 
-    _getAllFilters: function () {
-      var a = [];
-      var oVM = this.getView().getModel("vm");
+    onBeforeRebindL2: function (oEvent) {
+      var p = oEvent.getParameter("bindingParams");
+      p.filters = p.filters || [];
 
-      var k = oVM.getProperty("/navKeys") || {};
-      if (k.ICERunDate) a.push(new Filter("ICERunDate", FilterOperator.EQ, k.ICERunDate));
-      if (k.CompanyCode) a.push(new Filter("CompanyCode", FilterOperator.EQ, k.CompanyCode));
-      if (k.TradingPartner) a.push(new Filter("TradingPartner", FilterOperator.EQ, k.TradingPartner));
-      if (k.FinalBreakCode) a.push(new Filter("FinalBreakCode", FilterOperator.EQ, k.FinalBreakCode));
+      // nav keys (obavezni)
+      if (this._navKeys.ICERunDate) p.filters.push(new Filter("ICERunDate", FilterOperator.EQ, this._navKeys.ICERunDate));
+      if (this._navKeys.CompanyCode) p.filters.push(new Filter("CompanyCode", FilterOperator.EQ, this._navKeys.CompanyCode));
+      if (this._navKeys.TradingPartner) p.filters.push(new Filter("TradingPartner", FilterOperator.EQ, this._navKeys.TradingPartner));
+      if (this._navKeys.FinalBreakCode) p.filters.push(new Filter("FinalBreakCode", FilterOperator.EQ, this._navKeys.FinalBreakCode));
 
+      // SFB filters
       var oSfb = this.byId("sfbDetailL2");
       var aSfb = oSfb ? (oSfb.getFilters() || []) : [];
-      a = a.concat(aSfb);
+      p.filters = p.filters.concat(aSfb);
 
-      var d = oVM.getProperty("/drill") || {};
-      if (d.field && d.value) {
-        a.push(new Filter(d.field, FilterOperator.EQ, d.value));
+      // drill filter (field=value)
+      if (this._drill.field && this._drill.value) {
+        p.filters.push(new Filter(this._drill.field, FilterOperator.EQ, this._drill.value));
       }
-
-      return a;
     },
 
-    _rebindActiveSafe: function () {
-      if (!this._sfbReady) {
-        this._pendingRebind = true;
-        return;
-      }
-
-      var sKey = this.byId("itbViewsL2").getSelectedKey();
-      var m = { product: "stProductL2", segment: "stSegmentL2", caption: "stCaptionL2" };
-      var sId = m[sKey];
-
-      var oSt = this.byId(sId);
-      if (!oSt) { return; }
-
-      if (!this._stReady[sId]) {
-        this._pendingRebindBySt[sId] = true;
-        return;
-      }
-
-      oSt.rebindTable(true);
+    onSearch: function () {
+      var oSt = this.byId("stDetailL2");
+      oSt && oSt.rebindTable(true);
     },
-
-    onSearch: function () { this._rebindActiveSafe(); },
-    onTabSelect: function () { this._rebindActiveSafe(); },
 
     onNavBack: function () {
       var oHistory = History.getInstance();
