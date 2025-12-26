760f19691e96efa4cfb84137b7f3ed45912edacf
ICTFioneer <hopsiauto@gmail.com>
2025-12-26 13:46:21 +0100

    Update Detail.controller.js

--- Detail.controller.js
+++ Detail.controller.js
@@ -1,240 +1,196 @@
-// webapp/controller/Detail.controller.js
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/ui/core/routing/History"
-  ], function (Controller, JSONModel, Filter, FilterOperator, History) {
-    "use strict";
-  
-    return Controller.extend("ice.controller.Detail", {
-  
-      onInit: function () {
-        this.getView().setModel(new JSONModel({
-          navKeys: {
-            ICERunDate: null,
-            CompanyCode: "",
-            TradingPartner: "",
-            FinalBreakCode: ""
-          }
-        }), "vm");
-  
-        // readiness
-        this._sfbReady = false;
-        this._stReady = { stProduct: false, stSegment: false, stCaption: false };
-  
-        // pending rebind flags
-        this._pendingRebind = false;
-        this._pendingRebindBySt = { stProduct: false, stSegment: false, stCaption: false };
-  
-        // SmartFilterBar initialise
-        var oSfb = this.byId("sfbDetail");
-        if (oSfb) {
-          oSfb.attachInitialise(function () {
-            this._sfbReady = true;
-  
-            // popuni polja iz sessionStorage kad je SFB spreman
-            this._restoreSfbFromSession();
-  
-            // ako je neko tražio rebind ranije, sad ga okini
-            if (this._pendingRebind) {
-              this._pendingRebind = false;
-              this._rebindActiveSafe();
-            }
-          }.bind(this));
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator",
+  "sap/ui/core/routing/History",
+  "ice/util/DateUtil"
+], function (Controller, JSONModel, Filter, FilterOperator, History, DateUtil) {
+  "use strict";
+
+  return Controller.extend("ice.controller.Detail", {
+
+    onInit: function () {
+      this.getView().setModel(new JSONModel({
+        navKeys: {
+          ICERunDate: null,
+          CompanyCode: "",
+          TradingPartner: "",
+          FinalBreakCode: ""
         }
-  
-        // SmartTable initialise (za svaku tab tabelu)
-        this._attachSmartTableInit("stProduct");
-        this._attachSmartTableInit("stSegment");
-        this._attachSmartTableInit("stCaption");
-  
-        this.getOwnerComponent().getRouter()
-          .getRoute("RouteDetail")
-          .attachPatternMatched(this._onRouteMatched, this);
-  
-        var oSt = this.byId("stProduct");
-  if (oSt) {
-    oSt.attachInitialise(function () {
-      var oTable = oSt.getTable();
-      if (oTable && oTable.attachItemPress) {
-        oTable.attachItemPress(this.onProductRowPress, this);
-      }
-    }.bind(this));
-  }
-  
-      },
-  
-      _attachSmartTableInit: function (sId) {
-        var oSt = this.byId(sId);
-        if (!oSt) { return; }
-  
-        oSt.attachInitialise(function () {
-          this._stReady[sId] = true;
-  
-          // ako baš ovu tabelu čekamo da bi rebindovali, uradi sad
-          if (this._pendingRebindBySt[sId]) {
-            this._pendingRebindBySt[sId] = false;
-            // rebind samo ako je trenutno aktivan tab taj
+      }), "vm");
+
+      this._sfbReady = false;
+      this._stReady = { stProduct: false, stSegment: false, stCaption: false };
+      this._pendingRebind = false;
+      this._pendingRebindBySt = { stProduct: false, stSegment: false, stCaption: false };
+
+      // SFB init gate
+      var oSfb = this.byId("sfbDetail");
+      if (oSfb) {
+        oSfb.attachInitialise(function () {
+          this._sfbReady = true;
+
+          // popuni SFB iz MAIN session storage
+          this._restoreSfbFromSession("MAIN_SFB_FILTERS");
+
+          if (this._pendingRebind) {
+            this._pendingRebind = false;
             this._rebindActiveSafe();
           }
         }.bind(this));
-      },
-  
-      _onRouteMatched: function (oEvent) {
-        var oArgs = oEvent.getParameter("arguments") || {};
-        var oQuery = oArgs["?query"] || {};
-  
-        var sIso = decodeURIComponent(oQuery.iceRunDate || "");
-        var dIce = sIso ? new Date(sIso) : null;
-  
-        this.getView().getModel("vm").setProperty("/navKeys", {
-          ICERunDate: dIce && !isNaN(dIce.getTime()) ? dIce : null,
-          CompanyCode: decodeURIComponent(oArgs.CompanyCode || ""),
-          TradingPartner: decodeURIComponent(oArgs.TradingPartner || ""),
-          FinalBreakCode: decodeURIComponent(oArgs.FinalBreakCode || "")
-        });
-  
-        var sTab = (oQuery.tab ? decodeURIComponent(oQuery.tab) : "product") || "product";
-        this.byId("itbViews").setSelectedKey(sTab);
-  
-        // restore odmah ako je SFB spreman (inače će uraditi initialise handler)
-        if (this._sfbReady) {
-          this._restoreSfbFromSession();
+      }
+
+      // SmartTable init gate (3 taba)
+      this._attachSmartTableInit("stProduct");
+      this._attachSmartTableInit("stSegment");
+      this._attachSmartTableInit("stCaption");
+
+      this.getOwnerComponent().getRouter()
+        .getRoute("RouteDetail")
+        .attachPatternMatched(this._onRouteMatched, this);
+    },
+
+    _attachSmartTableInit: function (sId) {
+      var oSt = this.byId(sId);
+      if (!oSt) { return; }
+
+      oSt.attachInitialise(function () {
+        this._stReady[sId] = true;
+
+        if (this._pendingRebindBySt[sId]) {
+          this._pendingRebindBySt[sId] = false;
+          this._rebindActiveSafe();
         }
-  
-        this._rebindActiveSafe();
-      },
-  
-      onNavBack: function () {
-        var oHistory = History.getInstance();
-        var sPrevHash = oHistory.getPreviousHash();
-  
-        if (sPrevHash !== undefined) {
-          window.history.go(-1);
-        } else {
-          this.getOwnerComponent().getRouter().navTo("RouteMain", {}, true);
-        }
-      },
-  
-      onSearch: function () {
-        this._rebindActiveSafe();
-      },
-  
-      onTabSelect: function () {
-        this._rebindActiveSafe();
-      },
-  
-      _restoreSfbFromSession: function () {
+      }.bind(this));
+    },
+
+    _onRouteMatched: function (oEvent) {
+      var oArgs = oEvent.getParameter("arguments") || {};
+      var oQuery = oArgs["?query"] || {};
+
+      var dIce = DateUtil.fromIso(oQuery.iceRunDate || "");
+
+      this.getView().getModel("vm").setProperty("/navKeys", {
+        ICERunDate: dIce,
+        CompanyCode: decodeURIComponent(oArgs.CompanyCode || ""),
+        TradingPartner: decodeURIComponent(oArgs.TradingPartner || ""),
+        FinalBreakCode: decodeURIComponent(oArgs.FinalBreakCode || "")
+      });
+
+      var sTab = (oQuery.tab ? decodeURIComponent(oQuery.tab) : "product") || "product";
+      this.byId("itbViews").setSelectedKey(sTab);
+
+      // ako je SFB već spreman, restore odmah
+      if (this._sfbReady) {
+        this._restoreSfbFromSession("MAIN_SFB_FILTERS");
+      }
+
+      this._rebindActiveSafe();
+    },
+
+    onNavBack: function () {
+      var oHistory = History.getInstance();
+      var sPrevHash = oHistory.getPreviousHash();
+
+      if (sPrevHash !== undefined) {
+        window.history.go(-1);
+      } else {
+        this.getOwnerComponent().getRouter().navTo("RouteMain", {}, true);
+      }
+    },
+
+    onSearch: function () {
+      this._rebindActiveSafe();
+    },
+
+    onTabSelect: function () {
+      this._rebindActiveSafe();
+    },
+
+    _restoreSfbFromSession: function (sKey) {
+      var oSfb = this.byId("sfbDetail");
+      if (!oSfb) { return; }
+
+      try {
+        var sData = sessionStorage.getItem(sKey);
+        if (!sData) { return; }
+        oSfb.setFilterData(JSON.parse(sData), true);
+      } catch (e) {}
+    },
+
+    _rebindActiveSafe: function () {
+      if (!this._sfbReady) {
+        this._pendingRebind = true;
+        return;
+      }
+
+      var sKey = this.byId("itbViews").getSelectedKey();
+      var m = { product: "stProduct", segment: "stSegment", caption: "stCaption" };
+      var sId = m[sKey];
+
+      var oSt = this.byId(sId);
+      if (!oSt) { return; }
+
+      if (!this._stReady[sId]) {
+        this._pendingRebindBySt[sId] = true;
+        return;
+      }
+
+      oSt.rebindTable(true);
+    },
+
+    onBeforeRebindAny: function (oEvent) {
+      var oBindingParams = oEvent.getParameter("bindingParams");
+      oBindingParams.filters = (oBindingParams.filters || []).concat(this._getAllFilters());
+    },
+
+    _getAllFilters: function () {
+      var a = [];
+
+      var k = this.getView().getModel("vm").getProperty("/navKeys") || {};
+      if (k.ICERunDate) a.push(new Filter("ICERunDate", FilterOperator.EQ, k.ICERunDate));
+      if (k.CompanyCode) a.push(new Filter("CompanyCode", FilterOperator.EQ, k.CompanyCode));
+      if (k.TradingPartner) a.push(new Filter("TradingPartner", FilterOperator.EQ, k.TradingPartner));
+      if (k.FinalBreakCode) a.push(new Filter("FinalBreakCode", FilterOperator.EQ, k.FinalBreakCode));
+
+      var oSfb = this.byId("sfbDetail");
+      var aSfbFilters = oSfb ? (oSfb.getFilters() || []) : [];
+
+      return a.concat(aSfbFilters);
+    },
+
+    // XML ti već poziva itemPress=".onProductRowPress" na svim tabelama
+    onProductRowPress: function (oEvent) {
+      var oItem = oEvent.getParameter("listItem");
+      var oCtx = oItem && oItem.getBindingContext();
+      if (!oCtx) { return; }
+
+      // snimi DETAIL filter state (da se L2 popuni)
+      try {
         var oSfb = this.byId("sfbDetail");
-        if (!oSfb) { return; }
-  
-        try {
-          var sData = sessionStorage.getItem("MAIN_SFB_FILTERS");
-          if (!sData) { return; }
-  
-          var oData = JSON.parse(sData);
-          oSfb.setFilterData(oData, true);
-        } catch (e) {
-          // ignore
-        }
-      },
-  
-      _rebindActiveSafe: function () {
-        // 1) SFB gate: ako nije spreman -> zapamti i izađi
-        if (!this._sfbReady) {
-          this._pendingRebind = true;
-          return;
-        }
-  
-        var sKey = this.byId("itbViews").getSelectedKey();
-        var m = { product: "stProduct", segment: "stSegment", caption: "stCaption" };
-        var sId = m[sKey];
-  
-        var oSt = this.byId(sId);
-        if (!oSt) { return; }
-  
-        // 2) SmartTable gate: ako nije spreman -> zapamti za tu tabelu i izađi
-        if (!this._stReady[sId]) {
-          this._pendingRebindBySt[sId] = true;
-          return;
-        }
-  
-        // 3) Sve spremno -> rebind
-        oSt.rebindTable(true);
-      },
-  
-      onBeforeRebindAny: function (oEvent) {
-        var oBindingParams = oEvent.getParameter("bindingParams");
-        oBindingParams.filters = (oBindingParams.filters || []).concat(this._getAllFilters());
-      },
-  
-      _getAllFilters: function () {
-        var a = [];
-  
-        var k = this.getView().getModel("vm").getProperty("/navKeys") || {};
-        if (k.ICERunDate) a.push(new Filter("ICERunDate", FilterOperator.EQ, k.ICERunDate));
-        if (k.CompanyCode) a.push(new Filter("CompanyCode", FilterOperator.EQ, k.CompanyCode));
-        if (k.TradingPartner) a.push(new Filter("TradingPartner", FilterOperator.EQ, k.TradingPartner));
-        if (k.FinalBreakCode) a.push(new Filter("FinalBreakCode", FilterOperator.EQ, k.FinalBreakCode));
-  
-        // SFB je spreman (rebind ga garantuje), pa možemo getFilters
-        var oSfb = this.byId("sfbDetail");
-        var aSfbFilters = oSfb ? (oSfb.getFilters() || []) : [];
-        return a.concat(aSfbFilters);
-      },
-  
-onProductRowPress: function (oEvent) {
-  this._navToDetailL2(oEvent, "product", "TeamProductViewL2Set", "Product");
-},
+        sessionStorage.setItem("DETAIL_SFB_FILTERS", JSON.stringify((oSfb && oSfb.getFilterData()) || {}));
+      } catch (e) {}
 
-onSegmentRowPress: function (oEvent) {
-  this._navToDetailL2(oEvent, "segment", "TeamSegmentViewL2Set", "Segment");
-},
+      var oRow = oCtx.getObject() || {};
+      var sTab = this.byId("itbViews").getSelectedKey(); // product/segment/caption
 
-onCaptionRowPress: function (oEvent) {
-  this._navToDetailL2(oEvent, "caption", "TeamCategoryViewL2Set", "ICCategory");
-},
+      // jedan KeyValue zavisno od taba
+      var sRoute = "RouteDetailL2Product";
+      var sKeyValue = "";
 
-_navToDetailL2: function (oEvent, sViewKey, sEntitySetL2, sDrillField) {
-  var oItem = oEvent.getParameter("listItem");
-  var oCtx = oItem && oItem.getBindingContext();
-  if (!oCtx) { return; }
+      if (sTab === "product") {
+        sRoute = "RouteDetailL2Product";
+        sKeyValue = oRow.Product || "";
+      } else if (sTab === "segment") {
+        sRoute = "RouteDetailL2Segment";
+        sKeyValue = oRow.Segment || "";
+      } else {
+        sRoute = "RouteDetailL2Category";
+        sKeyValue = oRow.ICCategory || "";
+      }
 
-  var oRow = oCtx.getObject() || {};
-
-  var sIce = encodeURIComponent(this._toIso(oRow.ICERunDate));
-  var sCC  = encodeURIComponent(oRow.CompanyCode || "");
-  var sTP  = encodeURIComponent(oRow.TradingPartner || "");
-  var sFB  = encodeURIComponent(oRow.FinalBreakCode || "");
-  var sVal = encodeURIComponent(oRow[sDrillField] || "");
-
-  this.getOwnerComponent().getRouter().navTo("RouteDetailL2", {
-    ICERunDate: sIce,
-    CompanyCode: sCC,
-    TradingPartner: sTP,
-    FinalBreakCode: sFB,
-    "?query": {
-      view: sViewKey,          // product|segment|caption
-      es: sEntitySetL2,        // TeamProductViewL2Set | TeamSegmentViewL2Set | TeamCategoryViewL2Set
-      field: sDrillField,      // Product | Segment | ICCategory
-      value: sVal              // vrednost iz reda
-    }
-  });
-},
-
-  _toIso: function (v) {
-    if (!v) { return ""; }
-    if (v instanceof Date) { return v.toISOString(); }
-    var m = /\/Date\((\d+)\)\//.exec(String(v));
-    if (m && m[1]) {
-      var d = new Date(Number(m[1]));
-      return isNaN(d.getTime()) ? "" : d.toISOString();
-    }
-    return String(v);
-  }
-  
-  
-    });
-  });
+      this.getOwnerComponent().getRouter().navTo(sRoute, {
+        CompanyCode: oRow.CompanyCode || "",
+        TradingPartner: oRow.TradingPartner || "",
+        FinalBreakCode: oRow.FinalBreakC
